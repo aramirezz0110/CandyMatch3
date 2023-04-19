6e50818270c8f7e9dac0ca145911f69b600bc1d5
Arom Ramirez <aramirez@shokworks.io>
2023-04-18 23:12:42 -0400

    Board Manager script content added

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static BoardManager Instance;
 
-    // Update is called once per frame
-    void Update()
+    [Header("REFERENCES")]
+    public List<Sprite> prefabs = new List<Sprite>();
+    public GameObject currentCandy;
+    public Vector2Int size;
+
+    private GameObject[,] candies;
+    public bool isShifting { get; set; }
+
+    #region UNITY METHODS
+    private void Awake()
     {
-        
+        if (!Instance)
+            Instance = this;
+        else
+            Destroy(gameObject);
     }
+    private void Start()
+    {
+        Vector2 offset = currentCandy.GetComponent<BoxCollider2D>().size;
+        CreateInitialBoard(offset);
+    }
+    #endregion
+
+    #region PRIVATE METHODS
+    private void CreateInitialBoard(Vector2 offset)
+    {
+        candies = new GameObject[size.x, size.y];
+
+        float startX = this.transform.position.x;
+        float startY = this.transform.position.y;
+
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                GameObject newCandy = Instantiate(
+                    currentCandy,
+                    new Vector3(startX+(offset.x*x), startY+(offset.y*y), 0),
+                    Quaternion.identity
+                    );
+                newCandy.name = $"Candy[{x}][{y}]";
+                candies[x, y] = newCandy;
+            }
+        }
+    }
+    #endregion
 }
