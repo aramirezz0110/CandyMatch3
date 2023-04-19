b670a6b3065cbe0e20bf4daac6f80af8704629e6
Arom Ramirez <aramirez@shokworks.io>
2023-04-18 22:53:20 -0400

    Candy script added

---
+++ Assets/Scripts/Candy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
+public class Candy : MonoBehaviour
+{
+    private static Color selectedColor = new Color(0.5f, .5f, .5f, 1f);
+    private static Candy previousSelected = null;
+
+    private SpriteRenderer spriteRenderer;
+    private bool isSelected = false;
+
+    public int id;
+
+    private Vector2[] adjacentDirections = new Vector2[] 
+    {
+        Vector2.up,
+        Vector2.down, 
+        Vector2.left,
+        Vector2.right,
+    };
+
+    #region UNITY METHODS
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    #endregion
+}
